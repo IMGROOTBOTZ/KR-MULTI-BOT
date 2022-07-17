2d1b5a848f5ef831b965a02a5bf9f265edbc7480
LᴀsᴛDʀᴏɢᴢ <105853088+Lastdrogs@users.noreply.github.com>
2022-07-17 19:18:51 +0530

    Create txt.py

---
+++ helper/txt.py
@@ -0,0 +1,156 @@
+class kr(object):
+    ID_TXT = """🔎  𝗬𝗢𝗨𝗥 𝗗𝗘𝗧𝗔𝗜𝗟𝗦 
+
+๏ 𝗧𝗚 𝗜𝗗 : <code>{message.from_user.id}</code>
+๏ 𝗗𝗖 : <code>{message.from_user.dc_id}</code>
+๏ 𝗙𝗜𝗥𝗦𝗧 𝗡𝗔𝗠𝗘 : <code>{message.from_user.first_name}<code>
+๏ 𝗨𝗦𝗘𝗥 𝗡𝗔𝗠𝗘 : @{message.from_user.username}
+๏ 𝗧𝗚 𝗟𝗜𝗡𝗞 : <code>https://telegram.me/{message.from_user.username}</code>
+
+Tʜᴀɴᴋ Yᴏᴜ Fᴏʀ Usɪɴɢ Mᴇ❣️ """
+
+    ABOUT_TXT = """<b>
+╔════❰ 𝗠𝗨𝗟𝗧𝗜 𝗕𝗢𝗧 ❱═❍
+║╭━━━━━━━━━━━━━━━➣
+║┣⪼🤖 ᴍʏ ɴᴀᴍᴇ : {}
+║┣⪼👦 Oᴡɴᴇʀ : <a href=https://t.me/tamil_kid>ђ ค ℘ ℘ ꪗ 💥 Ҝ|ᗪ</a>
+║┣⪼👨‍💻 Dᴇᴠ : <a href=https://t.me/LastDrogz>Lᴀsᴛ 🐲 Dʀᴏɢᴢ</a>
+║┣⪼📢 Uᴘᴅᴀᴛᴇ : <a href=https://t.me/kr_botz>𝗞𝗥 ⚠︎ 𝗕ᴏᴛᴢ</a>
+║┣⪼❣️ Sᴜᴘᴘᴏʀᴛ : <a href=https://t.me/kr_join>𝗞𝗥 👽 𝗝ᴏɪɴ</a>
+║┣⪼📡 Hᴏsᴛᴇᴅ Oɴ : <a href=https://dashboard.heroku.com>Hᴇʀᴏᴋᴜ</a>
+║┣⪼🗣️ Lᴀɴɢᴜᴀɢᴇ : <a href=https://www.python.org>Pʏᴛʜᴏɴ3</a>
+║┣⪼📚 Lɪʙʀᴀʀʏ : <a href=https://github.com/pyrogram>Pʏʀᴏɢʀᴀᴍ</a>  
+║┣⪼🗒️ Vᴇʀsɪᴏɴ : V {}
+║╰━━━━━━━━━━━━━━━➣
+╚═════❰ @KR_Botz ❱═════❍ </b>"""
+
+    HELP_TXT = """
+<b><u>𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦</u></b>
+
+**◉ Sᴇɴᴅ Cʜᴀɴɴᴇʟ Lᴀsᴛ Mᴇssᴀɢᴇ Wɪᴛʜ
+  Fᴏʀᴡᴇʀᴅ Tᴀɢ Tᴏ Gᴇᴛ Tʜᴇ Cʜᴀɴɴᴇʟ ɪᴅ 💯
+
+◉ /id - Yᴏᴜʀ Tɢ Iᴅ & Iɴғᴏ
+
+◉ /telegraph - Rᴇᴘʟʏ Tᴏ Bᴇʟᴏᴡ 𝟻ᴍʙ Mᴇᴅɪᴀ
+  Tᴏ Gᴇᴛ Tᴇʟᴇɢʀᴀᴘʜ Lɪɴᴋ💯
+
+◉ /stickerid - Rᴇᴘʟʏ Tᴏ Aɴʏ Sᴛɪᴄᴋᴇʀ Tᴏ Gᴇᴛ Sᴛɪᴄᴋᴇʀ ID
+
+Tᴏ Mᴀᴋᴇ Lᴏɢᴏ - /logo Your Nᴀᴍᴇ
+Tᴏ Mᴀᴋᴇ Sǫᴜᴀʀᴇ Lᴏɢᴏ -  /logosq Yᴏᴜʀ Nᴀᴍᴇ**
+
+♻️ Exᴀᴍᴘʟᴇ:
+`/logo KR`
+`/logosq BGM`
+
+**🤩Tʜᴀɴᴋs Fᴏʀ Usɪɴɢ Mᴇ😍
+
+®️ Mᴀᴅᴇ Wɪᴛʜ ❣️ @KR_Botz 
+⚜️ Bᴏᴛ Aɴʏ Issᴜᴇs Cᴏɴᴛᴀᴄᴛ Mᴇ
+𝗔𝗱𝗺𝗶𝗻 : @Tamil_KiD
+𝗦𝘂𝗽𝗽𝗼𝗿𝘁 : @KR_Join**
+"""
+
+    DON_TXT = """
+<b><u>𝗧𝗛𝗔𝗡𝗞𝗦 𝗙𝗢𝗥 𝗦𝗛𝗢𝗪𝗜𝗡𝗚 𝗜𝗡𝗧𝗘𝗥𝗘𝗦𝗧 𝗜𝗡 𝗗𝗢𝗡𝗔𝗧𝗜𝗢𝗡</u></b>
+
+<b>Dᴏɴᴀᴛᴇ Us Tᴏ Kᴇᴇᴘ Oᴜʀ Sᴇʀᴠɪᴄᴇs Cᴏɴᴛɪɴᴏᴜsʟʏ Aʟɪᴠᴇ 😢
+Yᴏᴜ Cᴀɴ Sᴇɴᴅ Aɴʏ Aᴍᴏᴜɴᴛ 
+
+Dᴏɴᴀᴛᴇ Oɴʟʏ Oɴᴇ Rᴜᴘᴇᴇ 🥲
+
+Of 10₹, 20₹, 30₹, 50₹, 70₹, 100₹, 200₹ 😊
+📨 Pᴀʏᴍᴇɴᴛ Mᴇᴛʜᴏᴅs:
+ 
+GᴏᴏɢʟᴇPᴀʏ / Pᴀʏᴛᴏɴ / PʜᴏɴPᴀʏ / PᴀʏPᴀʟ
+ 
+ Oʀ Dᴏɴᴀᴛᴇ: Mᴇssᴀɢᴇ Mᴇ @Tamil_KiD </b>
+"""
+
+    DEV_TXT = """
+<b><u>Special Thanks & Developers</b></u>
+
+• 🥇 [𝗕𝗘𝗧𝗔 👑 𝗕𝗢𝗧𝗭](https://github.com/Jeolpaul) 
+• 🏅 [𝙼𝚛.𝙼𝙺𝙽 𝚃𝙶](https://t.me/mr_MKN)
+• 🎖 [๑۩ tค๓เl ۞ التاميل ۩๑](https://t.me/tamil_kid)
+"""
+
+    PRI_TXT = """
+<b>
+Hᴇʏ! Dᴇᴀʀ Vɪsɪᴛᴏʀs 🚸
+I Cᴀɴ Cʀᴇᴀᴛᴇ A Pᴇʀsᴏɴᴀʟ Bᴏᴛ Fᴏʀ Yᴏᴜ Iɴ Vᴇʀʏ Mɪɴɪᴍᴜᴍ Rᴀᴛᴇ ;
+  
+            Jᴜsᴛ Pᴀʏ 💰 ₹𝟾𝟶/Bᴏᴛ Fᴏʀ Bᴏᴛs. [Sᴏᴍᴇ Bᴏᴛs Lɪᴋᴇ Mᴏᴠɪᴇs Bᴏᴛs Cᴀɴ Cʜᴀʀɢᴇ ₹𝟷𝟼𝟶 Wɪᴛʜ 1𝟶𝟶ᴋ Fɪʟᴇs. ] 
+
+🗣📲📞 Cᴏɴᴛᴀᴄᴛ  Mᴇ : @Tamil_KiD
+
+Pʟᴇᴀsᴇ!!!! Dᴏɴ'ᴛ Dᴇᴍᴀɴᴅs Oᴛᴛ Bᴏᴛs Oʀ Pʀᴇᴍɪᴜᴍ Dᴏᴡɴʟᴏᴀᴅᴇʀ Bᴏᴛs Eʟsᴇ Bᴀɴ!.... 
+
+               ~ Rᴇɢᴀʀᴅs ~
+ ✥════ @KR_Botz ════✥
+
+🤑 Dᴏɴᴀᴛɪᴏɴ Fᴏʀ Us Wʜᴏ Gɪᴠᴇs Cʜᴇᴀᴘᴇsᴛ Pʀɪᴄᴇ Bᴏᴛ :-
+
+ <code> happysfx24@ybl </code> or @Tamil_KiD </b>
+"""
+
+    TELE_TXT = """
+<b><u> 𝗧𝗘𝗟𝗘𝗚𝗥𝗔𝗣𝗛</u>
+
+ᴅᴏ ᴀs ʏᴏᴜ ᴡɪsʜ ᴡɪᴛʜ telegra.ph ᴍᴏᴅᴜʟᴇ!
+
+<u>𝗨𝗦𝗔𝗚𝗘</u>
+
+🤧 /telegraph - sᴇɴᴅ ᴍᴇ ᴘɪᴄᴛᴜʀᴇ ᴏʀ ᴠɪᴅᴇ ᴜɴᴅᴇʀ (5MB)
+
+<u>𝗡𝗢𝗧𝗘</u>
+
+• ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ɪs ᴀᴠᴀɪʟᴀʙʟᴇ ɪɴ ɢᴏᴜᴘs ᴀɴᴅ ᴘᴍs
+• ᴛʜɪs Command ᴄᴀɴ ʙᴇ ᴜsᴇᴅ ʙʏ ᴇᴠᴇʀʏᴏɴᴇ
+
+Mᴀᴅᴇ Bʏ <a href=https://t.me/KR_Botz>KR ⚠️ Bᴏᴛᴢ</a> 
+
+</b>
+"""
+
+    STICKER_TXT = """
+<b><u>𝗦𝗧𝗜𝗖𝗞𝗘𝗥𝗦 𝗜𝗗</u>
+
+ʏᴏᴜ ᴄᴀɴ ᴜsᴇ ᴛʜɪs ᴍᴏᴅᴜʟᴇ ᴛᴏ ғɪɴᴅ ᴀɴʏ ᴀɴʏ sᴛɪᴄᴋᴇʀs ɪᴅ. 
+
+• <u>𝗨𝗦𝗔𝗚𝗘</u>  
+
+Tᴏ Gᴇᴛ Sᴛɪᴄᴋᴇʀ ID
+ 
+  <u>⭕ 𝗛𝗢𝗪 𝗧𝗢 𝗨𝗦𝗘</u>
+ 
+◉ Rᴇᴘʟʏ Tᴏ Aɴʏ Sᴛɪᴄᴋᴇʀ /stickerid
+
+Mᴀᴅᴇ Bʏ <a href=https://t.me/KR_Botz>𝗞𝗥 ⚠️ 𝗕ᴏᴛᴢ</a> 
+</b>
+"""
+
+    FUN_TXT = """<b><u>𝗝𝗨𝗦 𝗧𝗘𝗦𝗧 𝗧𝗛𝗜𝗦 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 😉</u>
+
+◉ /runs         
+◉ /ikka      
+◉ /dice     
+◉ /arrow    
+◉ /goal    
+◉ /luck    
+◉ /throw     
+◉ /bowling  
+◉ /tenpins   
+
+Mᴀᴅᴇ Bʏ <a href=https://t.me/KR_Botz>𝗞𝗥 ⚠️ 𝗕ᴏᴛᴢ</a> </b>
+"""
+
+    SOR_TXT = """
+HII
+"""
+
+
+
+
+
